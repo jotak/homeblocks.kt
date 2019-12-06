1154b4bf58725b509c1fc09c287c2606a665570e
Joel Takvorian <jtakvori@redhat.com>
2019-12-06 14:36:32 +0100

    prod config

--- src/main/kotlin/net/homeblocks/server/Server.kt
+++ src/main/kotlin/net/homeblocks/server/Server.kt
@@ -3,16 +3,17 @@ package net.homeblocks.server
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.http.HttpServerOptions
+import io.vertx.core.net.PemKeyCertOptions
 import kotlinx.coroutines.runBlocking
 import net.homeblocks.oauth.getOAuthProviders
 import net.homeblocks.services.ProfileService
 import net.homeblocks.services.UserService
 import kotlin.concurrent.thread
 
-// const val securePort = 443
-// const val insecurePort = 80
-const val securePort = 8000
-const val insecurePort = 8001
+ const val securePort = 443
+ const val insecurePort = 80
+//const val securePort = 8000
+//const val insecurePort = 8001
 
 class Server: AbstractVerticle() {
   override fun start(startFuture: Future<Void>) {
@@ -27,10 +28,10 @@ class Server: AbstractVerticle() {
 
   private suspend fun initServer(userService: UserService, profileService: ProfileService, startFuture: Future<Void>) {
     val options = HttpServerOptions()
-//        .setSsl(true).setPemKeyCertOptions(PemKeyCertOptions()
-//        .setKeyPath("/etc/letsencrypt/live/www.homeblocks.net/privkey.pem")
-//        .setCertPath("/etc/letsencrypt/live/www.homeblocks.net/fullchain.pem")
-//    )
+        .setSsl(true).setPemKeyCertOptions(PemKeyCertOptions()
+        .setKeyPath("/etc/letsencrypt/live/www.homeblocks.net/privkey.pem")
+        .setCertPath("/etc/letsencrypt/live/www.homeblocks.net/fullchain.pem")
+    )
     val oAuthProviders = getOAuthProviders(userService.fsRoot, vertx)
     val router = Routes.create(vertx, userService, profileService, oAuthProviders)
 
